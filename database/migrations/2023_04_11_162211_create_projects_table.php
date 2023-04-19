738cec3647943c1959fd17b3bc6767845532307f
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-04-19 16:39:58 +0200

    second commit

--- database/migrations/2023_04_11_162211_create_projects_table.php
+++ database/migrations/2023_04_11_162211_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('technology', 50);
+            $table->string('technology', 50); 
             $table->text('description');
             $table->string('url', 100)->nullable();
             $table->string('image',)->nullable();
